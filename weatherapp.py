b161ab2a99c32df986651e57bcedfbbace83c3e2
Magdalena Nowowiejska <47540038+ArchyArch@users.noreply.github.com>
2020-05-14 20:54:19 +0200

    Delete old file

--- weatherapp.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-city = input('City name: ')
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=767573953473bdb871a80111df8b19af&units=metric'
-
-json_data = requests.get(url).json()
-formatted_data = json_data['main']['temp']
-formatted_data1 = json_data['weather'][0]['description']
-print(f'Temperature: {formatted_data}' + '°C, Weather: ' + formatted_data1)
